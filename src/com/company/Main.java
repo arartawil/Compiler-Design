36f061fd0babeb6eff3af53d8d6301b18ed2e47e
arartawil <arartawil@yahoo.com>
2018-04-02 10:51:28 +0300

    First one

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+import java.util.ArrayList;
+
+
+public class Main
+{
+    ArrayList<Token>token=new ArrayList<>();
+    public static void main(String[] args) throws Exception
+    {
+        new Language();
+        ReadCode2 readCode=new ReadCode2("Files/test.txt");
+
+
+
+    }
+
+
+}
